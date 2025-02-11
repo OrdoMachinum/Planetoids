ca74db9a8341ceffc247c1deb393741a2d38ac19
riddick@Spartacus <laca103@gmail.com>
2025-02-11 19:51:37 +0100

    Basic functionality

---
+++ celestialview.cpp
@@ -0,0 +1,24 @@
+#include "celestialview.h"
+#include <string_view>
+
+
+CelestialView::CelestialView(const std::string& initialName, const Color& initialColor) :
+    m_name{initialName}, m_color{initialColor}
+{
+
+}
+
+std::string_view CelestialView::getName() const
+{
+    return m_name;
+}
+
+void CelestialView::setName(const std::string & newName)
+{
+    m_name = newName;
+}
+
+Color& CelestialView::getColor()
+{
+    return m_color;
+}
